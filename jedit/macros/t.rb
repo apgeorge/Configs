0260bd087a5059bac56647b9d0ee15497f166652
Aroj George <apgeorge@thoughtworks.com>
2012-01-12 20:45:44 +0000

    Adding jedit configs and custom macros

---
+++ jedit/macros/t.rb
@@ -0,0 +1,24 @@
+class M
+    def method_name
+      puts 1
+      new
+      puts 5
+    end
+    
+    def new
+        if x
+a            puts 2
+            puts 3
+            puts 4
+        end
+    end
+    
+    def a
+      puts 2
+      puts 3
+      puts 4
+      
+    end
+    
+    
+end
